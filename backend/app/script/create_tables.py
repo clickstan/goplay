7e904544ab528880e55249833a0413674a0bc7b9
Juan Ruiz <juan.jose.rgz@gmail.com>
2010-05-31 23:53:42 -0700

    q mula..

---
+++ backend/app/script/create_tables.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import sys
+import hashlib
+
+sys.path.extend(['../', './'])
+
+import db
+from db.models import *
+
+
+@db.uses_session
+def setDefaults(session):
+    roles = {'privileged-client' : 'The GoPlay Original Frontend',
+             'admin' : 'An administrator of the GoPlay server',
+             'player' : 'Just a player'}
+
+    roles = [user.Role(name, description)
+                for name, description
+                    in roles.iteritems()]
+
+    session.add_all(roles)
+
+    privileged_client_role = None
+    for role in roles:
+        if role.name == 'privileged-client':
+            privileged_client_role = role
+
+    # ---
+    for username in ['admin','player','other']:
+        privileged_user1 = user.User(
+                        username,
+                        hashlib.sha256(username).hexdigest(),
+                        'The GoPlay Original Frontend')
+
+        privileged_user1.role = privileged_client_role
+
+        session.add(privileged_user1)
+    # ---
+    privileged_user1 = user.User(
+                        'tgpof',
+                        hashlib.sha256('#sp1-goplay').hexdigest(),
+                        'The GoPlay Original Frontend')
+
+    privileged_user1.role = privileged_client_role
+
+    session.add(privileged_user1)
+
+    # ---
+    
+    privileged_user2 = user.User(
+                        'registrador',
+                        hashlib.sha256('registrador').hexdigest(),
+                        'el que registra a la mara')
+
+    privileged_user2.role = privileged_client_role
+
+    session.add(privileged_user2)
+    
+    # ---    
+
+    session.commit()
+    
+    
+    
+
+
+def main():
+    db.Base.metadata.drop_all(db.engine)
+    db.Base.metadata.create_all(db.engine)
+    setDefaults()
+
+if __name__ == "__main__":
+    main()
---
+++ frontend/libcomm/src/comm/protocol/server/Room.as
@@ -0,0 +1,59 @@
+// ActionScript file
+package comm.protocol.server {
+	import comm.protocol.server.Util;
+	
+	import flash.utils.ByteArray;
+	
+	public class Room	{
+	
+		public static function add_game(wplayer:String, bplayer:String, size:int, room:String):Object {
+			return {'command' : 'room.add_game',
+				'wplayer':wplayer,
+				'bplayer':bplayer,
+				'size':size,
+				'room':room,
+				'trans' : Util.nextTrans()};
+		}
+		
+		public static function get_all_rooms():Object {
+			return {'command' : 'room.get_all_rooms',
+				'trans'	  : Util.nextTrans()};
+		}
+		public static function get_public_games_list(room:String):Object {
+			return {'command' : 'room.get_public_games_list',
+					'room':room,
+					'trans'    : Util.nextTrans()}
+		}
+		public static function request_public_game(room:String, color:String, size:int):Object {
+			return {'command'  : 'room.request_public_game',
+				'color'	   : color,
+				'size'	   : size,
+				'room'	   : room,
+				'trans'    : Util.nextTrans()};
+		}
+		
+		public static function getUsersFromRoom(name:String):Object {
+			return {'command' : 'room.getUsersFromRoom',
+				'name'		: name,
+				'trans'	  : Util.nextTrans()};
+		}
+		
+		public static function createRoom(name:String):Object {
+			return {'command' : 'room.createRoom',
+				'name'		: name,
+				'trans'	  : Util.nextTrans()};
+		}
+
+		public static function openRoom(name:String):Object {
+			return {'command' : 'room.openRoom',
+				'name'		: name,
+				'trans'	  : Util.nextTrans()};
+		}
+		
+		public static function getChatId(roomName:String):Object{
+			return {'command' : 'room.getChatId',
+				'roomName'		: roomName,
+				'trans'	  : Util.nextTrans()};
+		}
+	}
+}
